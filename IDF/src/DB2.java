896b69a25125cc2f34678d47f49811cfdc73ddc0
richa293 <richadesai293@gmail.com>
2018-04-21 00:50:10 -0400

    Add files via upload

---
+++ IDF/src/DB2.java
@@ -0,0 +1,27 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.*;
+
+public class DB2 
+{
+    Connection con =null;
+    public static Connection ConnectDB()
+    {
+        try
+        {
+            Class.forName("org.sqlite.JDBC");
+            Connection con=DriverManager.getConnection("jdbc:sqlite:C:\\Users\\urmikapoor\\Desktop\\IDF2.sqlite");
+            JOptionPane.showMessageDialog(null, "ConnectionEstablished");
+            return con;
+        }
+        catch(Exception e)
+        {
+            JOptionPane.showMessageDialog(null,e);
+        }
+        return null;    
+    }
+    public static void main(String args[])
+    {
+        ConnectDB();
+    }
+}
